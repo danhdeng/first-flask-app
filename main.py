d0e2bffc93e088fb3e82b4dfc018b11c8cd47227
Dan Deng <hui_danny_deng@hotmail.com>
2021-07-26 23:20:28 -0400

    add db

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+class Item(db.model):
+    name = db.Comlumn(db.String(length=32), nullable=False, unique=True)
+    barcode = db.Comlumn(db.String(length=12))
+    price = db.Column(db.Integer(), nullable=False)
+
 
 @app.route('/')
 @app.route('/home')
@@ -9,10 +17,10 @@ def home_page():
 
 
 @app.route('/market')
-def market():
+def market_page():
     items = [
         {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
         {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
         {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
     ]
-    return render_template("market.html", items =items)
+    return render_template("market.html", items=items)
